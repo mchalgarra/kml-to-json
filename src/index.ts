4dfe30e467743ea89235b0db5c4e63de6fe185de
Michell Algarra <michell.algarra@gmail.com>
2021-10-27 14:51:33 -0300

    feat(index.ts): implemented a simple flow to test the json conversion

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log('Hello World!')
+import { convertToJson } from './utils/converter'
+import { readKML } from './utils/reader'
+
+console.log('Hello world!')
+
+setTimeout(() => {
+  const input = document.getElementById('kml-input') as HTMLInputElement
+
+  if (input) {
+    input.addEventListener('change', async () => {
+      if (input.files && input.files[0]) {
+        const file = input.files[0]
+
+        const kmlText = await readKML(file)
+        console.log(kmlText)
+
+        const json = convertToJson(kmlText)
+        console.log(JSON.parse(json))
+      }
+    })
+  }
+}, 100)
