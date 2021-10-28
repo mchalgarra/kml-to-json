b820542717a2d062a6ddf800795dfaa9f0324551
Michell Algarra <michell.algarra@gmail.com>
2021-10-28 11:07:12 -0300

    feat(index.ts): implemented two functions to be exported and used in other files, one to convert kml into a json object and other to convert kml into a json string

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,29 @@
+import { Kml } from './interfaces/kml.interface'
 import { convertToJson } from './utils/converter'
 import { readKML } from './utils/reader'
 
-console.log('Hello world!')
+/**
+ * Converts a KML file to a JSON object.
+ *
+ * @param file The KML file to be uploaded.
+ * @returns An object containing all KML information.
+ */
+export async function kmlToJson(file: File): Promise<Kml> {
+  const kmlText = await readKML(file)
+  const json = convertToJson(kmlText)
+  return json
+}
+
+/**
+ * Converts a KML file to a stringified JSON object.
+ *
+ * @param file The KML file to be uploaded.
+ * @returns A stringified object containing all KML information.
+ */
+export async function kmlToJsonString(file: File): Promise<string> {
+  const json = await kmlToJson(file)
+  return JSON.stringify(json)
+}
 
 setTimeout(() => {
   const input = document.getElementById('kml-input') as HTMLInputElement
@@ -11,11 +33,12 @@ setTimeout(() => {
       if (input.files && input.files[0]) {
         const file = input.files[0]
 
-        const kmlText = await readKML(file)
-        console.log(kmlText)
+        const json = await kmlToJsonString(file)
 
-        const json = convertToJson(kmlText)
-        console.log(JSON.parse(json))
+        const div = document.createElement('div')
+        div.innerHTML = json
+
+        document.body.appendChild(div)
       }
     })
   }
