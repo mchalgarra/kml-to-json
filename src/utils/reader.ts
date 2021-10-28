4d283c1067436a1d500733e2dae910d1d3d9bb6a
Michell Algarra <michell.algarra@gmail.com>
2021-10-28 11:04:38 -0300

    feat(utils/reader.ts): added an error thrown when no file is uploaded or its type is not kml

--- src/utils/reader.ts
+++ src/utils/reader.ts
@@ -3,7 +3,19 @@
  *
  * @param file The file that will be read.
  * @returns The file text content.
+ * @throws Whether no file is uploaded or the file is not a KML.
  */
 export async function readKML(file: File): Promise<string> {
+  if (!file) {
+    throw new Error('No file uploaded!')
+  }
+
+  if (
+    file.type !== 'application/vnd.google-earth' &&
+    !file.name.match(/\.kml$/)
+  ) {
+    throw new Error('Invalid file type! The file must be a KML.')
+  }
+
   return await file.text()
 }
